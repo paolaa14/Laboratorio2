1e0963191cbab771587c3b7e6aed47b8d2fa8437
andre13002 <est.andrea.blandon@unimilitar.edu.co>
2025-02-12 22:38:32 -0500

    Add files via upload

--- grafica convolucion.py
+++ grafica convolucion.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 # Señales dadas en la imagen
 x_n = [5, 6, 0, 0, 5, 0, 1]
 h_n = [1, 0, 1, 3, 0, 9, 7, 6, 2, 0]
@@ -13,23 +12,27 @@ y_n = np.convolve(x_n, h_n)
 ecuacion = "y(n) = " + " + ".join(f"{y_n[i]}x^{i}" for i in range(len(y_n)))
 print(ecuacion)
 
+# Crear las figuras separadas
+plt.figure(figsize=(8, 4))
+plt.stem(range(len(x_n)), x_n, basefmt=" ", linefmt='b-', markerfmt='bo')
+plt.xlabel('n')
+plt.ylabel('x(n)')
+plt.title('Señal x(n)')
+plt.grid()
+plt.show()
 
-# Señales de entrada
-x = np.array([5, 6, 0, 0, 5, 0, 1])
-h = np.array([1, 0, 1, 3, 0, 9, 7, 6, 2, 0])
+plt.figure(figsize=(8, 4))
+plt.stem(range(len(h_n)), h_n, basefmt=" ", linefmt='r-', markerfmt='ro')
+plt.xlabel('n')
+plt.ylabel('h(n)')
+plt.title('Señal h(n)')
+plt.grid()
+plt.show()
 
-# Cálculo de la convolución
-y = np.convolve(x, h)
-
-# Configuración de la figura
-plt.figure(figsize=(8, 5))
-
-# Gráfica de la convolución
-plt.stem(range(len(y)), y, basefmt=" ")
-
+plt.figure(figsize=(10, 4))
+plt.stem(range(len(y_n)), y_n, basefmt=" ", linefmt='g-', markerfmt='go')
 plt.xlabel('n')
 plt.ylabel('y(n)')
 plt.title('Gráfica de la convolución y(n)')
 plt.grid()
-
 plt.show()
